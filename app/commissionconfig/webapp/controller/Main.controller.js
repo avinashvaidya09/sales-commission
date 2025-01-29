736760a29bcf0eeab2d3d5fd2aa0a71a8e8e4a05
Avinash Vaidya <avinash.vaidya@sap.com>
2025-01-29 19:51:13 +0000

    Added formatter and filter

--- app/commissionconfig/webapp/controller/Main.controller.js
+++ app/commissionconfig/webapp/controller/Main.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], (Controller, MessageToast, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, MessageToast, JSONModel, Filter, FilterOperator) => {
     "use strict";
 
     return Controller.extend("ns.commissionconfig.controller.Main", {
@@ -14,7 +16,8 @@ sap.ui.define([
             const oModel = this.getOwnerComponent().getModel();
             const oView = this.getView();
             const oParameters = {
-                $count: true
+                $count: true,
+                $expand: "status"
             }
             oModel.bindList("/CommissionConfig",undefined, undefined, undefined, oParameters).requestContexts().then((aContexts) => {
                 const aData = aContexts.map((oContext) => oContext.getObject());
@@ -23,6 +26,27 @@ sap.ui.define([
             }).catch((oError) => {
                 console.error("Error loading CommissionConfig data:", oError);
             });
+        },
+        formatDate: function (sDateTime) {
+            if (!sDateTime) {
+                return "";
+            }
+            const oDate = new Date(sDateTime);
+            return oDate.toISOString().split("T")[0]; // Formats as YYYY-MM-DD
+        },
+        onFilterConfig: function(oEvent) {
+            // Build filter array
+            const aFilter = [];
+            const sQuery = oEvent.getParameter("query");
+            if(sQuery) {
+                aFilter.push(new Filter("year", FilterOperator.Equals, sQuery));
+                aFilter.push(new Filter("title", FilterOperator.Contains, sQuery));
+            }
+
+            // Filter Binding
+            const oList = this.byId("commissionConfigTable");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
 
     });
