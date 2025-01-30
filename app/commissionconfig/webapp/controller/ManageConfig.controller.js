dbdccead448acf0d74b617726f714df2ad3eac83
Avinash Vaidya <avinash.vaidya@sap.com>
2025-01-30 16:11:06 +0000

    Added validations on the create page

--- app/commissionconfig/webapp/controller/ManageConfig.controller.js
+++ app/commissionconfig/webapp/controller/ManageConfig.controller.js
@@ -1,12 +1,32 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], (Controller, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+], (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     return Controller.extend("ns.commissionconfig.controller.ManageConfig", {
         onInit() {
+            this._fetchCommissionConfigStatus();
+        },
+        _fetchCommissionConfigStatus: function() {
+            const oModel = this.getOwnerComponent().getModel();
+            const oView = this.getView();
 
+            // Fetch Status Data from OData
+            oModel.bindList("/CommissionConfigStatus").requestContexts().then((aContexts) => {
+                const aStatusData = aContexts.map((oContext) => oContext.getObject());
+                const oStatusModel = new JSONModel({ Statuses: aStatusData });
+                // Set the model for the view
+                oView.setModel(oStatusModel, "statusModel");
+                // Set Default Selection to "PEND"
+                const oStatusSelect = oView.byId("statusSelect");
+                if (oStatusSelect) {
+                    oStatusSelect.setSelectedKey("PEND");
+                }
+            }).catch((oError) => {
+                console.error("Error loading status values:", oError);
+            });
         },
         onSave: function () {
             const oModel = this.getOwnerComponent().getModel();
@@ -40,8 +60,22 @@ sap.ui.define([
                 MessageToast.show(sErrorMessage);
                 console.error("Create failed:", oError);
             })
-
+        },
+        onBack: function () {
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteMain");
+        },
+        onYearChange: function (oEvent) {
+            const oInput = oEvent.getSource();
+            const sValue = oInput.getValue();
             
+            // Allow only numbers & ensure it's exactly 4 digits
+            if (!/^\d{4}$/.test(sValue)) {
+                oInput.setValueState("Error");
+                oInput.setValueStateText("Year must be a 4-digit number");
+            } else {
+                oInput.setValueState("None"); // Remove error state if valid
+            }
         }
     });
 });
